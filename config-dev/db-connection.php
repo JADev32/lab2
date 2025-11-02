17dd9588d2e8aa6c2e3aa589f95e2d667b34897c
Julian Aladio <alladio64@gmail.com>
2025-11-02 19:03:34 -0300

    cambio-dbconnection

--- config-dev/db-connection.php
+++ config-dev/db-connection.php
@@ -1,3 +1,7 @@
 <?php
-
-return new PDO("mysql:host=localhost;dbname=sample", "sampleuser", "samplepass", [PDO::ATTR_PERSISTENT => true]);
+return new PDO(
+    "mysql:host=mysql-service-ec2;dbname=app_db", 
+    "root", 
+    "password", 
+    [PDO::ATTR_PERSISTENT => true]
+);
