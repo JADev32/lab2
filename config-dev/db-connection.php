70ef5d1f431de8c3a88d2adf173618b1966a0df5
Julian Aladio <alladio64@gmail.com>
2025-11-02 21:49:24 -0300

    prueba-connection

--- config-dev/db-connection.php
+++ config-dev/db-connection.php
@@ -1,6 +1,6 @@
 <?php
 return new PDO(
-    "mysql:host=mysql-service-ec2.ecs-internal;dbname=app_db", 
+    "mysql:host=10.0.10.249;dbname=app_db", 
     "root", 
     "password", 
     [PDO::ATTR_PERSISTENT => true]
